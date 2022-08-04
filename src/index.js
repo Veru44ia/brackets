62338996b8dd41e4af55ec9c6ea4be3f3154467b
Veru44ia <vera3188@gmail.com>
2022-08-04 15:29:29 +0300

    fix: change solution to fix bugs

--- src/index.js
+++ src/index.js
@@ -1,32 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  const OPEN_BRACKETS = ['(', '{', '[', '|'];
-  const BRACKETS_PAIR = {
-    [')']: '(',
-    ['}']: '{',
-    [']']: '[',
-    ['|']: '|',
-  };
+  const OPEN_BRACKETS = [];
+  bracketsConfig.forEach((item) => OPEN_BRACKETS.push(item[0]));
+
+  const BRACKETS_PAIR = {};
+  bracketsConfig.forEach((item) => (BRACKETS_PAIR[item[1]] = item[0]));
 
   let stack = [];
 
   for (let i = 0; i < str.length; i++) {
     let currentSymbol = str[i];
-      
-    if (OPEN_BRACKETS.includes(currentSymbol)) {
+    let topElement = stack[stack.length - 1];
+
+    if (stack.length !== 0 && BRACKETS_PAIR[currentSymbol] === topElement) {
+      stack.pop();
+    } else if (OPEN_BRACKETS.includes(currentSymbol)) {
       stack.push(currentSymbol);
     } else {
-      if (stack.length === 0) {
-        return false;
-      }
-
-      let topElement = stack[stack.length - 1];
-
-      if (BRACKETS_PAIR[currentSymbol] === topElement) {
-        stack.pop();
-      } else {
-        return false;
-      }
+      return false;
     }
   }
   return stack.length === 0;
-}
+};
